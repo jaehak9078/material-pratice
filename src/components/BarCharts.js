8e5f2fcedfcdf849f6bd7ad3beeca8c9d88ef7af
jaehak <jaehak9078@gmail.com>
2021-01-14 10:22:48 +0900

    v4

--- src/components/BarCharts.js
+++ src/components/BarCharts.js
@@ -1,30 +1,32 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import * as am4core from "@amcharts/amcharts4/core";
 import * as am4charts from "@amcharts/amcharts4/charts";
 import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+import axios from 'axios';
 
 am4core.useTheme(am4themes_animated);
 
-const data = [
-    { date: "1월", revenue: 4215 },
-    { date: "2월", revenue: 5312 },
-    { date: "3월", revenue: 6251 },
-    { date: "4월", revenue: 7841 },
-    { date: "5월", revenue: 9821 },
-    { date: "6월", revenue: 14984 },
-  ];
+
 
 
 const BarCharts = () => {
     const chart = useRef(null);
+  
+    
+
     useEffect(()=>{
         chart.current = am4core.create("chartdiv",am4charts.XYChart);
         chart.current.paddingRight = 20;
-        chart.current.data = data;
+
+        axios.get('/api/chart').then((res)=>{
+            console.log(res.data);
+            chart.current.data = res.data;
+        })
+        
 
         let categoryAxis = chart.current.xAxes.push(new am4charts.CategoryAxis());
         categoryAxis.renderer.grid.template.location = 0;
-        categoryAxis.dataFields.category = "date";
+        categoryAxis.dataFields.category = "month";
         categoryAxis.renderer.minGridDinstance = 40;
         categoryAxis.fontSize = 11;
 
@@ -35,9 +37,9 @@ const BarCharts = () => {
         valueAxis.renderer.minGridDinstance = 30;
 
         let series = chart.current.series.push(new am4charts.ColumnSeries());
-        series.dataFields.categoryX = "date";
-        series.dataFields.valueY = "revenue";
-        series.columns.template.tooltipText = "{valueY.revenue}";
+        series.dataFields.categoryX = "month";
+        series.dataFields.valueY = "value";
+        series.columns.template.tooltipText = "{valueY.value}";
         series.columns.template.tooltipY = 0;
         series.columns.template.strokeOpacity = 0;
 
--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -1,21 +1,42 @@
-import React from 'react';
-import {Chart,PieSeries} from '@devexpress/dx-react-chart-material-ui';
+import React, { useEffect, useRef } from 'react';
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+import axios from 'axios';
 ;
 
-const data = [
-    {color:'Red', area: 8.32},
-    {color:'Blue', area: 12.21},
-    {color:'Green', area: 15.58},
-    {color:'Yellow', area: 11.25},
-];
+am4core.useTheme(am4themes_animated);
 
 const PieChart = () => {
+    const pieChart = useRef(null);
+    useEffect(()=>{
+
+        pieChart.current = am4core.create("chartdiv3", am4charts.PieChart);
+
+        axios.get('/api/chart')
+        .then((res)=>{
+            pieChart.current.data = res.data;
+        })
+        
+        let pieSeries = pieChart.current.series.push(new am4charts.PieSeries());
+        pieSeries.dataFields.value = "value";
+        pieSeries.dataFields.category = "month";
+        pieSeries.slices.template.stroke = am4core.color("#fff");
+        pieSeries.slices.template.strokeWidth = 2;
+        pieSeries.slices.template.strokeOpacity = 1;
+
+        pieSeries.hiddenState.properties.opacity = 1;
+        pieSeries.hiddenState.properties.endAngle = -90;
+        pieSeries.hiddenState.properties.startAngle = -90;
+
+        return()=>{
+            !pieChart && pieChart.current.dispose();
+        }
+    },[]);
+
     return (
-        <div>
-            <Chart data={data}>
-                
-                <PieSeries valueField="area" argumentField="color" />
-            </Chart>
+        <div id="chartdiv3" style={{width:"100%",height:"500px"}}>
+            
         </div>
     );
 };
